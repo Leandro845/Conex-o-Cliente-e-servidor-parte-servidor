3b014e8d89058a96dd7e63c5aa11230cef9a0f84
Leandro845 <113248888+Leandro845@users.noreply.github.com>
2022-09-21 22:43:30 -0300

    Update README.py

--- README.py
+++ README.py
@@ -4,5 +4,21 @@ Parte servidor
 
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = '0.0.0.0' #Server ip
+port = 8888 #Server port
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create the socket
+sock.bind((ip, port)) #Slect port and address
+sock.getpeername() #Return server ip
+sock.listen() #The server starts listening
+
+data, address = sock.accept() #The server accepts the connections
+print(f'host: {address}')
+
+while True:
+    d = data.recv(9000) #Receive the data
+    d.decode() #Decode de data
+
+    print(d)
+
 
